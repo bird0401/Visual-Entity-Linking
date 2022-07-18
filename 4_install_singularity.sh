49802870454a502ba1270d680de8a56064b3d579
bird <53257509+bird0401@users.noreply.github.com>
2022-07-18 12:45:37 +0900

    Update 4_install_singularity.sh

--- 4_install_singularity.sh
+++ 4_install_singularity.sh
@@ -1,18 +1,10 @@
-
 #!/bin/sh
 
 sudo git clone https://github.com/sylabs/singularity.git
-
 cd singularity
-
 sudo git fetch --all
-
 sudo git checkout 2.5.0
-
 sudo ./autogen.sh
-
 sudo ./configure --prefix=/usr/local
-
 make
-
 sudo make install
