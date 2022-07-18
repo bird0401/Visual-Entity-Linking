2f70111bbdbf022119b205f309782878a2d5882e
bird <53257509+bird0401@users.noreply.github.com>
2022-07-18 16:22:15 +0900

    Update 4_install_singularity.sh

--- 4_install_singularity.sh
+++ 4_install_singularity.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
+sudo apt-get update && sudo apt-get -y install python dh-autoreconf build-essential libarchive-dev
+
 sudo git clone https://github.com/sylabs/singularity.git
 cd singularity
 sudo git fetch --all
-sudo git checkout 2.5.0
+# sudo git tag -l
+sudo git checkout 2.5.2
 sudo ./autogen.sh
-sudo ./configure --prefix=/usr/local
-make
+sudo ./configure --prefix=/usr/local --sysconfdir=/etc
+sudo make
 sudo make install
