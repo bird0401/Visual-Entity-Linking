3ab724a48108f71b2f6298b31e3c5b5efd159efd
bird <53257509+bird0401@users.noreply.github.com>
2022-09-13 16:20:09 +0900

    Update 1_git.sh

--- 1_git.sh
+++ 1_git.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 sudo apt-get install git-all
-git clone https://github.com/bird0401/Instance_level_recognition.git
+sudo git clone https://github.com/bird0401/Instance_level_recognition.git
