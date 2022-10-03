0bfe79dd95ef10ef369af00559ad286909ee16cc
bird0401 <mhiro7777777@gmail.com>
2022-10-03 20:55:41 +0000

    add: field name, terminator, enclosure, and delimiter

--- app/scraping/table2csv.sh
+++ app/scraping/table2csv.sh
@@ -11,8 +11,11 @@ sudo docker exec db3 bash -c "chmod 777 /tmp/csv"
 
 sudo docker exec db3 mysql -p${MYSQL_PASS} \
     -e "USE scraping_dog_breeds_by_name;" \
-    -e "SELECT * FROM img_path INTO OUTFILE '/tmp/csv/img_path.csv';" \
-    -e "SELECT * FROM names INTO OUTFILE '/tmp/csv/names.csv';" 
-
+    -e "SELECT 'id', 'path' 
+        UNION SELECT * FROM img_path INTO OUTFILE '/tmp/csv/img_path.csv' 
+        FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '\"' LINES TERMINATED BY '\n';" \
+    -e "SELECT 'id', 'name' 
+        UNION SELECT * FROM names INTO OUTFILE '/tmp/csv/names.csv' 
+        FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '\"' LINES TERMINATED BY '\n';"
 sudo docker cp b4ef6fed4c41:/tmp/csv . \
 && sudo chmod 755 csv
