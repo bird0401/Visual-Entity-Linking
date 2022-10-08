622507811f6b37eed0d11ec03b368207c8a14e4a
bird0401 <mhiro7777777@gmail.com>
2022-10-08 19:05:42 +0000

    add: regarding container files

--- app/ml/bin/image_push.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-export PROJECT=$(gcloud config list project --format "value(core.project)")
-docker build ../ -f Dockerfile -t "gcr.io/${PROJECT}/pytorch-custom:latest"
-docker push "gcr.io/${PROJECT}/tf-custom:latest"
