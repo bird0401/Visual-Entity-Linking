35e1bfdd12a6a21dd97013993dc9f1b50215239a
bird0401 <mhiro7777777@gmail.com>
2022-10-23 04:40:46 +0900

    update config, update data.py adding transform module

--- app/ml/src/data.py
+++ app/ml/src/data.py
@@ -1,15 +1,62 @@
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
+
 from torch.utils.data import Dataset
 import cv2
 import torch
 import traceback    
 
+class DataTransform():
+    def __init__(self, input_size, color_mean = [0.485, 0.456, 0.406], color_std = [0.229, 0.224, 0.225]):
+        self.data_transform = {
+            "train": A.Compose([
+                A.Resize(input_size, input_size),
+                A.ShiftScaleRotate(shift_limit=0.1, 
+                                scale_limit=0.15, 
+                                rotate_limit=60, 
+                                p=0.5),
+                A.HueSaturationValue(
+                        hue_shift_limit=0.2, 
+                        sat_shift_limit=0.2, 
+                        val_shift_limit=0.2, 
+                        p=0.5
+                    ),
+                A.RandomBrightnessContrast(
+                        brightness_limit=(-0.1,0.1), 
+                        contrast_limit=(-0.1, 0.1), 
+                        p=0.5
+                    ),
+                A.Normalize(
+                        mean=color_mean, 
+                        std=color_std, 
+                        max_pixel_value=255.0, 
+                        p=1.0
+                    ),
+                ToTensorV2()], p=1.),
+            
+            "valid": A.Compose([
+                A.Resize(input_size, input_size),
+                A.Normalize(
+                        mean=color_mean, 
+                        std=color_std, 
+                        max_pixel_value=255.0, 
+                        p=1.0
+                    ),
+                ToTensorV2()], p=1.)
+        }
+
+    def __call__(self, phase, img):
+        return self.data_transform[phase](img)
+
+
 class EntityLinkingDataset(Dataset):
-    def __init__(self, df, transforms=None):
+    def __init__(self, df, phase, transform=None):
         self.df = df
         # self.file_names = df['file_path'].values
         self.file_names = df['path'].values
         self.labels = df['label'].values
-        self.transforms = transforms
+        self.phase = phase
+        self.transform = transform
         
     def __len__(self):
         return len(self.df)
@@ -21,8 +68,8 @@ class EntityLinkingDataset(Dataset):
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         label = self.labels[index]
         
-        if self.transforms:
-            img = self.transforms(image=img)["image"]
+        if self.transform:
+            img = self.transform(phase=self.phase, image=img)
             
         return {
             'image': img,
