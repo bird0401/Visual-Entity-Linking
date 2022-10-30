a98abdebdc55460a4081db67a1e21b4e9f93b619
bird0401 <mhiro7777777@gmail.com>
2022-10-30 16:02:36 +0900

    feat: accuracy_score from sklearn

--- app/ml/src/train.py
+++ app/ml/src/train.py
@@ -4,12 +4,18 @@ import gc
 from torch.optim import lr_scheduler
 import torch.amp
 
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import f1_score
+
 def train_one_epoch(model, optimizer, scheduler, dataloader, device, epoch, n_accumulate, criterion, enable_amp_half_precision):
     model.train()
     
     dataset_size = 0
     running_loss = 0.0
     running_acc = 0.0
+    running_acc2 = 0.0
 
     bar = tqdm(enumerate(dataloader), total=len(dataloader))
     for step, data in bar:
@@ -22,37 +28,36 @@ def train_one_epoch(model, optimizer, scheduler, dataloader, device, epoch, n_ac
         loss = criterion(outputs, labels)
         loss = loss / n_accumulate
 
-# ADD
-        if enable_amp_half_precision:
-            with amp.scale_loss(loss, optimizer) as scaled_loss:
-                scaled_loss.backward()
-        else:
-            loss.backward()
-# until here
-    
+        # if enable_amp_half_precision:
+        #     with amp.scale_loss(loss, optimizer) as scaled_loss:
+        #         scaled_loss.backward()
+        # else:
+        
+        loss.backward()
+
         if (step + 1) % n_accumulate == 0:
             optimizer.step()
-
-            # zero the parameter gradients
             optimizer.zero_grad()
-
             if scheduler is not None:
                 scheduler.step()
             
         predicted = torch.max(outputs, 1)[1]
         running_loss += (loss.item() * batch_size)
         running_acc += (predicted == labels).sum().item()
+        running_acc2 += accuracy_score(labels, predicted)
         dataset_size += batch_size
         
         epoch_loss = running_loss / dataset_size
         epoch_acc = running_acc / dataset_size
+        epoch_acc2 = running_acc2 / dataset_size
+        
         
         # show each values on the progress bar
         bar.set_postfix(Epoch=epoch, Train_Loss=epoch_loss, Train_Acc=epoch_acc,
                         LR=optimizer.param_groups[0]['lr'])
     gc.collect()
     
-    return epoch_loss, epoch_acc
+    return epoch_loss, epoch_acc, epoch_acc2
 
 @torch.inference_mode()
 def valid_one_epoch(model, dataloader, device, epoch, criterion, optimizer):
@@ -61,6 +66,7 @@ def valid_one_epoch(model, dataloader, device, epoch, criterion, optimizer):
     dataset_size = 0
     running_loss = 0.0
     running_acc = 0.0
+    running_acc2 = 0.0
     
     bar = tqdm(enumerate(dataloader), total=len(dataloader))
     for step, data in bar:        
@@ -75,17 +81,19 @@ def valid_one_epoch(model, dataloader, device, epoch, criterion, optimizer):
         predicted = torch.max(outputs, 1)[1]
         running_loss += (loss.item() * batch_size)
         running_acc += (predicted == labels).sum().item()
+        running_acc2 += accuracy_score(labels, predicted)
         dataset_size += batch_size
         
         epoch_loss = running_loss / dataset_size
         epoch_acc = running_acc / dataset_size
+        epoch_acc2 = running_acc2 / dataset_size
         
         bar.set_postfix(Epoch=epoch, Valid_Loss=epoch_loss, Valid_Acc=epoch_acc,
                         LR=optimizer.param_groups[0]['lr'])   
     
     gc.collect()
     
-    return epoch_loss, epoch_acc
+    return epoch_loss, epoch_acc, epoch_acc2
 
 
 def fetch_scheduler(optimizer, scheduler, T_max, T_0, min_lr):
