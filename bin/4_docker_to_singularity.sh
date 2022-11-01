3601f6f34938a0e8d1b066b0cfc60879914d3880
root <root@scraping.asia-northeast1-b.c.root-beanbag-354111.internal>
2022-11-01 12:56:34 +0000

    feat: docker to singulrirty

--- bin/4_docker_to_singularity.sh
+++ bin/4_docker_to_singularity.sh
@@ -5,6 +5,3 @@
 sudo docker login
 sudo docker tag $1 $2/$3:$4
 sudo docker push $2/$3:$4
-
-sudo apt-get install squashfs-tools
-sudo singularity pull docker://mot1536/docker-python_python3
---
+++ bin/6_pull_singularity.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+sudo apt-get install squashfs-tools
+sudo singularity pull docker://mot1536/python_ml
