87ecca659435506a8fb694efbe62d48c29af8902
bird0401 <mhiro7777777@gmail.com>
2022-11-03 11:40:26 +0900

    feat: conflict

--- app/ml/bin/hcp_job.sh
+++ app/ml/bin/hcp_job.sh
@@ -1,9 +1,28 @@
 #!bin/sh
 
+
+# IST-CLUSTER
+
 # $2: name of shellscript
 # $3: job number
 
-export PATH=/home/app/singularity/bin:$PATH
-sbatch ../bin/$2.sh
-cat slurm-$3.outsqueue
-squeue
+# export PATH=/home/app/singularity/bin:$PATH
+# sbatch ../bin/$2.sh
+# cat slurm-$3.outsqueue
+# squeue
+
+
+# WISTERIA
+
+# pjsub --interact -g gk77 -L rscgrp=interactive-a,node=1,jobenv=singularity
+# module load singularity/3.7.3
+# singularity shell gc_cli_latest.sif 
+# cd Instance_level_recognition/app/ml/
+# gcloud auth login
+# gsutil -m cp -r gs://entity_dogs_debug .
+# gcloud config set project $PROJECT_ID
+# mv entity_dogs_debug/ data/
+# singularity shell python_ml_latest.sif 
+# cd Instance_level_recognition/app/ml/src/
+# python main.py
+pjsub run.sh
