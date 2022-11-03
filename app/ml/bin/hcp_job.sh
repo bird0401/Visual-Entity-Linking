3f8e304666d673836ef113822006b2d569212bb2
bird0401 <mhiro7777777@gmail.com>
2022-11-03 11:36:43 +0900

    feat: run in wisteria

--- app/ml/bin/hcp_job.sh
+++ app/ml/bin/hcp_job.sh
@@ -3,7 +3,9 @@
 # $2: name of shellscript
 # $3: job number
 
-export PATH=/home/app/singularity/bin:$PATH
-sbatch ../bin/$2.sh
-cat slurm-$3.outsqueue
-squeue
+# export PATH=/home/app/singularity/bin:$PATH
+# sbatch ../bin/$2.sh
+# cat slurm-$3.outsqueue
+# squeue
+
+pjsub run.sh
