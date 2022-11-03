2fa6d833b0660ce6e90ea333b76ef2a9e7e861f3
bird0401 <mhiro7777777@gmail.com>
2022-11-03 13:38:47 +0900

    fix run in wisteria

--- app/ml/bin/run.sh
+++ app/ml/bin/run.sh
@@ -2,10 +2,13 @@
 
 #PJM -L rscgrp=debug-a
 #PJM -L node=1
-#PJM -L elapse=0:15:00
+#PJM -L elapse=0:30:00
 #PJM -L jobenv=singularity
 #PJM -g gk77
 #PJM -j
 
-module load singularity/2.5.2
-singularity exec --nv ../python_ml.simg python main.py
+module load singularity/3.7.3
+singularity exec \
+    --pwd /$HOME/Instance_level_recognition/app/ml/src/ \
+    --nv /$HOME/Instance_level_recognition/app/ml/python_ml_latest.sif \
+    python ./main.py
