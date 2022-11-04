fd4cd635ea2a1cff37d70de822c67e26fa7108ef
bird0401 <mhiro7777777@gmail.com>
2022-11-04 14:37:39 +0900

    feat config debug

--- app/ml/bin/hcp_memo.sh
+++ app/ml/bin/hcp_memo.sh
@@ -16,13 +16,15 @@
 
 # pjsub --interact -g gk77 -L rscgrp=interactive-a,node=1,jobenv=singularity
 # module load singularity/3.7.3
+
 # singularity shell gc_cli_latest.sif 
 # cd Instance_level_recognition/app/ml/
 # gcloud auth login
-# gsutil -m cp -r gs://entity_dogs_debug .
 # gcloud config set project $PROJECT_ID
+# gsutil -m cp -r gs://entity_dogs_debug .
 # mv entity_dogs_debug/ data/
-# singularity shell python_ml_latest.sif 
+
+# singularity shell /$HOME/Instance_level_recognition/app/ml/python_ml_latest.sif 
 # cd Instance_level_recognition/app/ml/src/
 # python main.py
 pjsub run.sh
