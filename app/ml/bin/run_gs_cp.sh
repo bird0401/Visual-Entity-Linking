8fd9261c81e6f9d525571f0b149f75ad1626037d
bird0401 <mhiro7777777@gmail.com>
2022-11-04 11:01:55 +0900

    add run gs cp on wisteria

---
+++ app/ml/bin/run_gs_cp.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+#PJM -L rscgrp=short-a
+#PJM -L node=1
+#PJM -L jobenv=singularity
+#PJM -g gk77
+#PJM -j
+
+module load singularity/3.7.3
+singularity exec \
+    --pwd /$HOME/Instance_level_recognition/app/ml/ \
+    /$HOME/Instance_level_recognition/app/ml/gc_cli_latest.sif \
+    gsutil -m cp -r gs://entity_dogs/ .
