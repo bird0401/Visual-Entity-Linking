ac293323d211d99e596ecb625bfee4d0f86c705c
bird0401 <mhiro7777777@gmail.com>
2023-02-03 08:17:12 +0000

    fix: install vim and tmux in Dockerfile, update: requirements

--- app/scraping/bin/container_setting.sh
+++ app/scraping/bin/container_setting.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
 
-apt update; apt -y upgrade
-apt-get install vim
 vim /usr/local/lib/python3.10/site-packages/fake_useragent/utils.py
 # before: html = html.split('<table class="w3-table-all notranslate">')[1]
 # after: html = html.split('<table class="ws-table-all notranslate">')[1]
-apt install tmux -y
 
-pip install PyYAML
