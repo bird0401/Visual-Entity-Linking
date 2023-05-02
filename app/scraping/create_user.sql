778cc9d7774c10012654dfc3ebdda6a5e4b4e98b
bird0401 <mhiro7777777@gmail.com>
2023-05-02 22:20:41 +0900

    feat: extract text and image from wikipedia

--- app/scraping/create_user.sql
+++ app/scraping/create_user.sql
@@ -1,6 +1,6 @@
 -- command to execute this file
 -- mysql -p < create_user.sql
 
-
+-- Define $MYSQL_ROOT_PASSWORD previously
 CREATE USER 'scraper' IDENTIFIED BY "$MYSQL_ROOT_PASSWORD";
 GRANT ALL ON *.* TO 'scraper';
